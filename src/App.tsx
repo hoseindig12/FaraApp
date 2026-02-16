78666cd24633b7b934826367b0ef1a97b53c8a74
hoseinx@gmail.com <hoseinx@gmail.com>
2026-02-16 19:11:47 +0330

    add font date picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container, Grid, TextField } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs, { Dayjs } from "dayjs";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Home() {
+  const [value, setValue] = useState<Dayjs | null>(dayjs());
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container>
+      <Grid container spacing={2} mt={4}>
+        <Grid item xs={12} md={6}>
+          <DatePicker
+            dis
+            label="تاریخ"
+            value={value}
+            format="YYYY/MM/DD"
+            onChange={(newValue) => setValue(newValue)}
+            slotProps={{
+              textField: {
+                fullWidth: true,
+              },
+            }}
+          />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
-export default App
+function About() {
+  return <div>About Page</div>;
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,48 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
+import dayjs from "dayjs";
+import jalaliday from "jalaliday";
+
+dayjs.extend(jalaliday);
+dayjs.calendar("jalali");
+
+// 👇 مهم
+document.body.setAttribute("dir", "rtl");
+
+const theme = createTheme({
+  direction: "rtl",
+  typography: {
+    fontFamily: "IRANSans, Vazirmatn, sans-serif",
+  },
+});
+
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <CacheProvider value={cacheRtl}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="fa">
+        <App />
+      </LocalizationProvider>
+    </ThemeProvider>
+  </CacheProvider>,
+);
